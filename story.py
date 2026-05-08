b58879b9e3d2ae5d3066e238a84601e345c8f804
FaisalA-dev <AnwariF475@gmail.com>
2026-05-08 12:12:50 -0400

    Added game intro and route choices

--- story.py
+++ story.py
@@ -1,2 +1,11 @@
 def start_story():
-    print("story")
+    print("FLEE THE FACILITY!")
+    print("Your eyes open and you find yourself in a room you've never been before. You're confused but you see there are guards outside the door. You need to find a way out of this place.")
+    print("Your eyes wonder around and you see your in a room but it's unlocked you believe a guard forgot to lock it.")
+    
+    
+    
+    print("You now have to make a choice on what to do next. Do you want to next...")
+    print("1. Figure out a way to impersonate a security guard and sneak out?")
+    print("2. Make a run for it into a dark hallway and hope to find an exit?")
+    print("3. Figure out a way to access a terminal and override the security system to open the doors without an alert?")
