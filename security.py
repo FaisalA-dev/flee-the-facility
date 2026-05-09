8dcf78e896aef837a773566b50fc96c0a22c40b4
FaisalA-dev <AnwariF475@gmail.com>
2026-05-08 20:51:26 -0400

    Update

--- security.py
+++ security.py
@@ -1,3 +1,22 @@
-def log_events(event):
+from datetime import datetime
+import hashlib
+
+def log_event(event):
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     with open("audit_log.txt", "a") as file:
-        file.write(event + "\n")
+        file.write(f"{time} - {event}\n")
+
+def save_game(route):
+    with open("save_game.txt", "w") as file:
+        file.write(route)
+
+def load_game():
+    try:
+        with open("save_game.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
+    
+def create_hash(data):
+    return hashlib.sha256(data.encode()).hexdigest()
